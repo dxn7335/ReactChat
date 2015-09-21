cd1cbcdf4c18ec7f0a3823ea4d37f98aee0f6600
Danny <dnguyen@sapient.com>
2015-09-21 17:14:52 -0400

    Starting Project

---
+++ js/components/login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+let Login = React.createClass({
+    render(){
+        console.log('login');
+        return (<div>Welcome to Login</div>);
+    }
+});
+        
+export default Login;
