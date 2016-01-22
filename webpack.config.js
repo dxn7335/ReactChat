f3e191c2cf78aa9470c0eb8701c27a9d85888f55
Danny <dnguyen@sapient.com>
2016-01-22 15:58:04 -0500

    Add underscore-template-loader for htmlwebpack-plugin
    Needed in order for title or other templating syntax to work on html
    files loaded/generated through plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,39 @@
 var webpack = require('webpack'); 
+var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
+var PATH = {
+                app: path.resolve( __dirname, 'src/js/app.js'),
+                build: path.resolve( __dirname, 'build')
+           }
+           console.log(PATH.build);
+
 module.exports = {  
-    entry: [
-      'webpack/hot/only-dev-server',
-      "./src/js/app.js"
-    ],
+    entry: {
+        app: process.env.NODE_ENV === 'production' ? [PATH.app] : ['webpack/hot/dev-server', PATH.app]
+    },
     output: {
-        path: __dirname + '/build',
+        path: PATH.build,
         filename: "bundle.js"
     },
     module: {
         loaders: [
             { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
             { test: /\.js$/, exclude: /node_modules/, loaders: ['react-hot','babel-loader']},
-            { test: /\.scss$/, loader: "style!css!sass" }
+            { test: /\.scss$/, loader: "style!css!sass" },
         ]
     },
     plugins: [
         new webpack.NoErrorsPlugin(),
         new HtmlWebpackPlugin({
-            title: 'ReactChat',
-            template: 'src/index.html'
+            title: 'React-Chat',
+            template: 'underscore-template-loader!./src/index.html',
+            inject: 'body'
         })
     ]
 
 };
+
+loaders: [
+            {test: /\.js?$/, exclude: /node_modules/, loader: 'babel',  query: {presets:['react', 'es2015']} }
+        ]
